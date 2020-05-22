84963f30e2922c69408aa303a26d8ca41103e7d9
chenzhuo <chenzhuo_249@163.com>
2020-05-22 12:49:21 +0800

    修改 tools.py

--- PYMARA/Public/tools.py
+++ PYMARA/Public/tools.py
@@ -2,19 +2,21 @@ from django.http import JsonResponse
 import json
 
 
-def error(code, msg=''):
+def error(code, msg):
     try:
-        data = json.dumps(msg)
-    except:
-        data = str(msg)
-    return JsonResponse({'code': code, 'error': data})
+        JsonResponse({'code': code, 'error': msg})
+    except Exception as e:
+        print(e)
+        print('---转换失败,上面是报错信息,请联系高---')
+        return JsonResponse({'code': code, 'error':'转JSON的时候出错了' })
 
 
 def good(msg=''):
     if not msg:
         return JsonResponse({'code':200})
     try:
-        data = json.dumps(msg)
-    except:
-        data = str(msg)
-    return JsonResponse({'code': 200, 'data': data})
+        return JsonResponse({'code': 200, 'data': msg})
+    except Exception as e:
+        print(e)
+        print('---转换失败,上面是报错信息,请联系高---')
+        return JsonResponse({'code': 200, 'data': '转JSON的时候出错了'})
